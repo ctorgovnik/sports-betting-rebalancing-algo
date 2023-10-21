76b9d7e155ed43f0cb142cad1a517f9fd1f3fa4d
cjt76 <cjt76@cornell.edu>
2023-10-20 23:12:11 -0400

    added profits accumulator and store average in dictionary

--- rebalancing_simulation.py
+++ rebalancing_simulation.py
@@ -5,29 +5,32 @@ class SimulateK:
     def  __init__(self, c, r, a):
       self.c = c
       self.r = r
-      self.qa = 0
-      self.qb = 0
+      # self.qa = 0
+      # self.qb = 0
       self.a = a
       self.profits = {}
 
-    def simulate(self, trials, beta, betb, random_probabilities, pa = 0, pb =0):
+    def simulate(self, trials, beta, betb, pa = 0, pb = 0):
   
        
        for k in range(2, 7):
+          profit_acc = np.arry()
           for i in range(trials):
-             if (random_probabilities):
-                pa = np.random.uniform(0, 100)
-                pb = 1 - self.pa
-             self.qa = self.a*self.pa
-             self.qb = self.a*self.pb
-             la = beta / self.qa
-             lb = betb / self.qb
-             self.qa = SimulateK.update(la)
-             self.qb = SimulateK.update(lb)
-             if np.random.randn > pa:
-                profit = beta - lb
-             else:
-                profit = betb - la
+            pa = np.random.uniform(0, 100)
+            pb = 1 - self.pa
+            qa = self.a*pa
+            qb = self.a*pb
+            la = beta / qa
+            lb = betb / qb
+            qa = SimulateK.update(la)
+            qb = SimulateK.update(lb)
+            if np.random.randn > pa:
+              profit = beta - lb
+            else:
+              profit = betb - la
+            profit_acc.add(profit)
+          
+          self.profits.update({k : np.mean(profit_acc)})
               
   
              
